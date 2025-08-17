c33350f1545f94a88766de7f3f5c6c128e1c2905
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-08-17 11:44:04 -0300

    chore: register `@adonisjs/static` middleware in kernel for static file serving

--- start/kernel.ts
+++ start/kernel.ts
@@ -27,6 +27,7 @@ server.use([
   () => import('#middleware/force_json_response_middleware'),
   () => import('@adonisjs/cors/cors_middleware'),
   () => import('@adonisjs/vite/vite_middleware'),
+  () => import('@adonisjs/static/static_middleware')
 ])
 
 /**
