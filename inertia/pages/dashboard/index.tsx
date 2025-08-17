b1a07a388465c359d9d2990f977415eef3880539
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-08-17 13:20:06 -0300

    chore: fix typo in dashboard imports and format layout structure in index.tsx

--- inertia/pages/dashboard/index.tsx
+++ inertia/pages/dashboard/index.tsx
@@ -1,14 +1,21 @@
 import { Head } from '@inertiajs/react'
-import { Header } from '~fures/dashboard/components/Header'
-import { Sidebar } from '~/fe~u/dashboard/components/Sidebar'
-import { DashboardContent } from '~/featur~/hboard/components/DashboardContent'
+import { Header } from '~/features/dashboard/components/Header'
+import { Sidebar } from '~/features/dashboard/components/Sidebar'
+import { DashboardContent } from '~/features/dashboard/components/DashboardContent'
 
 export default function Dashboard() {
   return (
     <>
       <Head title="Dashboard - YOL Benício" />
-      <div className="flex h-screen bg-[#F1F1F2]"> [#F1F1F2]="flex-1 flex flex-col overflow-hidden">
+      <div className="flex h-screen bg-[#F1F1F2]">
+        <Sidebar />
+        <div className="flex-1 flex flex-col overflow-hidden">
           <Header />
           <main className="flex-1 overflow-y-auto">
-            <DashboardCont/main>
-    
+            <DashboardContent />
+          </main>
+        </div>
+      </div>
+    </>
+  )
+}
